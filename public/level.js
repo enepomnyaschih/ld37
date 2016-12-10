f8bc1f8f7f27b15ec6367e7da74bb096919f81b1
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2016-12-10 16:59:05 +0600

    Implemented unit movement method.

--- public/level.js
+++ public/level.js
@@ -1,11 +1,21 @@
 SR.Level = function(size) {
 	SR.Level._super.call(this);
+	this.tick = new JW.Property(0);
 	this.matrix = new SR.Matrix(size);
 	this.obstacles = new JW.ObservableArray();
 	this.units = new JW.ObservableArray();
+	this.paused = new JW.Property(true);
+	this.own(this.paused.$$mapObject(function(paused) {
+		return paused ? null : new JW.Interval(this.onTick, this, 1000 / SR.tickPerSecond);
+	}, this));
 };
 
-JW.extend(SR.Level, JW.Class);
+JW.extend(SR.Level, JW.Class, {
+	onTick: function() {
+		this.tick.set(this.tick.get() + 1);
+		this.units.each(JW.byMethod("move", [this]));
+	}
+});
 
 /*
 Значения матрицы
--- public/unit.js
+++ public/unit.js
@@ -2,10 +2,31 @@ SR.Unit = function(config) {
 	SR.Unit._super.call(this);
 	this.ij = new JW.Property(config.ij); // SR.Vector
 	this.direction = new JW.Property(config.direction); // number
-	this.movement = new JW.Property(0); // number, процент движения из клетки A в клетку B
+	this.movement = new JW.Property(0); // number, процент движения из клетки A в клетку B, от -1 до 0
 	this.controllable = config.controllable; // boolean
 	this.type = config.type; // SR.UnitType
+	this.ijTarget = new JW.Property(); // SR.Vector
+	this.path = []; // Array<number>
 };
 
 JW.extend(SR.Unit, JW.Class, {
+	move: function(level) {
+		var movement = this.type.speed;
+		while (movement && (this.path.length || this.movement.get() !== 0)) {
+			var remainingMovement = -this.movement.get();
+			if (movement < remainingMovement) {
+				this.movement.set(this.movement.get() + movement);
+				movement = 0;
+			} else if (this.path.length) {
+				movement -= remainingMovement;
+				this.movement.set(-1);
+				var direction = this.path[0];
+				this.path.splice(0, 1);
+				this.ij.set(SR.Vector.add(this.ij.get(), SR.dir4[direction]));
+				this.direction.set(direction);
+			} else {
+				this.movement.set(0);
+			}
+		}
+	}
 });
